3be2ce7e0fbb243a0387b37fa9f402de4a51cd8d
LavanyaAshwin <139799760+LavanyaAshwin@users.noreply.github.com>
2023-07-17 23:01:03 +0530

    Add files via upload
    Day1

---
+++ day1/Firefox.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Firefox {
+	
+	public static void main(String[] args) {
+		
+		float version=100.2f;
+		String name="firefox";
+		boolean isVisible=true;
+		int year=1998;
+		char logo='M';
+		
+		System.out.println("Whats the version ? :"+version);
+		System.out.println("name");
+		System.out.println("Release Year :"+year);
+		System.out.println("Logo is :"+logo);
+		System.out.println("Is it visible:"+isVisible );
+		
+		
+		
+	}
+
+}
