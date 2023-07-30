e2ff9b34653605cbb16dfd961b9c8429eafedd68
LavanyaAshwin <139799760+LavanyaAshwin@users.noreply.github.com>
2023-07-30 13:58:19 +0530

    Add files via upload

---
+++ department/Department.java
@@ -0,0 +1,21 @@
+package org.department;
+
+import org.college.College;
+
+public class Department extends College {
+
+	public void deptName() {
+		System.out.println("Manufacturing");
+	}
+	
+//	public static void main(String[] args) {
+//		
+//		Department d =new Department();
+//		d.deptName();
+//		d.collegeCode();
+//		d.collegeName();
+//		d.collegeRank();
+//		
+//	}
+}
+
