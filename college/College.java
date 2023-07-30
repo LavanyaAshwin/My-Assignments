a0031791feddedfb2b6eb7292004361fdf8e370e
LavanyaAshwin <139799760+LavanyaAshwin@users.noreply.github.com>
2023-07-30 13:57:53 +0530

    Add files via upload

---
+++ college/College.java
@@ -0,0 +1,22 @@
+package org.college;
+
+//collegeName(),collegeCode(),collegeRank()
+
+public class College {
+	
+	public void collegeName() {
+
+          System.out.println("ABC");
+	}
+	
+	public void collegeCode() {
+		System.out.println("12345");
+	}
+	
+	public void collegeRank() {
+		System.out.println("First");
+	}
+
+}
+
+
